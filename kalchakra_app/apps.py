4c59501cdf236485d1ced6bb96d594a0b63963bb
Hello-Iam-Deve <abhijeetjoshi1304@gmail.com>
2025-06-23 08:49:19 +0530

    Initial commit

---
+++ kalchakra_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KalcharaAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kalchakra_app'
---
+++ kalchakra_app/urls.py
@@ -0,0 +1,6 @@
+from kalchakra_app import views
+from django.urls import path
+
+urlpatterns = [
+    path('',views.home,name='home')
+]
